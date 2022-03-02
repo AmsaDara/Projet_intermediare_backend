510dcb4b6207a8e0b86f49918205184b41cb44d8
amsadara <thiiba95@gmail.com>
2022-03-02 16:24:25 +0000

    iniéme commit gérer les routes

--- controllers/routes/main.router.js
+++ controllers/routes/main.router.js
@@ -3,41 +3,41 @@ const route=express.Router();
 const articleServices= require('../../services/articles.service');
 const userCtrl = require('../../services/user.service.js');
 
-route.get('/',async (req,res)=>{
-    let articles=await articleServices.getAllArticles();
-    res.render('accueil', {articles});
-})
+// route.get('/',async (req,res)=>{
+//     let articles=await articleServices.getAllArticles();
+//     res.json('accueil', {articles});
+// })
 
 // CHEMIN D'AUTHENTIFICATION
 route.get('/signup',(req, res, next)=>{
-    res.render('pages/signup');
+    res.json('Connexion Réussie');
     next;
 })
 route.post('/signup',userCtrl.signup);
 
 // CHEMIN DE CONNEXION
 route.get('/login',(req,res,next)=>{
-    res.render('pages/login');
+    res.json('Merci de se connecter');
 })
 route.post('/login',userCtrl.login);
 route.get('/login',(req,res,next)=>{
-    res.render('accueil');
+    res.json('');
 })
 
 //CHEMIN POUR LES SERVICE ARTICLE
 route.get('/article',async (req,res)=>{
     let articles=await articleServices.getAllArticles();
-    res.render('pages/article',{articles});
+    res.json({articles});
 })
 route.post('/article', articleServices.saveArticle);
 
-//A PROPOS
-route.get('/apropos',(req,res)=>{
-    res.render('pages/apropos');
-})
-//CONTACT
-route.get('/contact',async (req,res)=>{
-    res.render('pages/contact');
-});
+// //A PROPOS
+// route.get('/apropos',(req,res)=>{
+//     res.json('pages/apropos');
+// })
+// //CONTACT
+// route.get('/contact',async (req,res)=>{
+//     res.json('pages/contact');
+// });
 
 module.exports=route;
